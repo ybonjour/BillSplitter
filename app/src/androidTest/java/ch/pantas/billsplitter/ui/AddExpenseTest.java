273363714a5a87ef82a564ebb3849c43a6d0cc84
gigerdo <dominik.giger@gmail.com>
2014-10-20 10:41:52 +0200

    ADDED: Test for edit expense

--- app/src/androidTest/java/ch/pantas/billsplitter/ui/AddExpenseTest.java
+++ app/src/androidTest/java/ch/pantas/billsplitter/ui/AddExpenseTest.java
@@ -11,6 +11,10 @@ import org.hamcrest.TypeSafeMatcher;
 import org.mockito.Mock;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 import ch.pantas.billsplitter.dataaccess.AttendeeStore;
@@ -19,6 +23,7 @@ import ch.pantas.billsplitter.dataaccess.ExpenseStore;
 import ch.pantas.billsplitter.dataaccess.ParticipantStore;
 import ch.pantas.billsplitter.dataaccess.UserStore;
 import ch.pantas.billsplitter.framework.BaseEspressoTest;
+import ch.pantas.billsplitter.framework.CustomViewMatchers;
 import ch.pantas.billsplitter.model.Attendee;
 import ch.pantas.billsplitter.model.Event;
 import ch.pantas.billsplitter.model.Expense;
@@ -29,8 +34,11 @@ import ch.pantas.billsplitter.ui.adapter.AttendeeAdapter;
 import ch.pantas.billsplitter.ui.adapter.PayerAdapter;
 import ch.yvu.myapplication.R;
 
+import ch.pantas.billsplitter.framework.CustomViewMatchers.*;
+
 import static ch.pantas.billsplitter.framework.CustomViewAssertions.hasBackgroundColor;
-import static ch.pantas.billsplitter.ui.ExpensesList.ARGUMENT_EVENT_ID;
+import static ch.pantas.billsplitter.ui.EventDetails.ARGUMENT_EVENT_ID;
+import static ch.pantas.billsplitter.ui.AddExpense.ARGUMENT_EXPENSE_ID;
 import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
 import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
 import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.typeText;
@@ -169,6 +177,64 @@ public class AddExpenseTest extends BaseEspressoTest<AddExpense> {
         verify(userStore, times(0)).persist(any(User.class));
         verify(expenseStore, times(0)).persist(any(Expense.class));
         onView(withId(R.id.expense_amount)).check(hasBackgroundColor(R.color.error_color));
+    }
+
+    @LargeTest
+    public void testEditExpenseLoadsExistingValues() {
+        // Given
+        User payer = new User("payerId", "Payer");
+        Expense expense = new Expense("expenseId", event.getId(), payer.getId(), "desc", 123.0);
+
+        User attendee1 = new User("att1", "attendee1");
+        User attendee2 = new User("att2", "attendee2");
+        User nonAttendee1 = new User("nonatt1", "nonAttendee1");
+        User nonAttendee2 = new User("nonatt2", "nonAttendee2");
+
+        List<User> allUsers = new LinkedList<User>();
+        allUsers.add(payer);
+        allUsers.add(attendee1);
+        allUsers.add(attendee2);
+        allUsers.add(nonAttendee1);
+        allUsers.add(nonAttendee2);
+
+        List<User> nonPayerList = new LinkedList<User>();
+        nonPayerList.add(attendee1);
+        nonPayerList.add(attendee2);
+        nonPayerList.add(nonAttendee1);
+        nonPayerList.add(nonAttendee2);
+
+        List<User> attendeeList = new LinkedList<User>();
+        attendeeList.add(payer);
+        attendeeList.add(attendee1);
+        attendeeList.add(attendee2);
+
+        Set<User> attendeeSet = new HashSet<User>(attendeeList);
+
+        for (User user : allUsers) {
+            when(userStore.getById(user.getId())).thenReturn(user);
+        }
+        when(expenseStore.getById(expense.getId())).thenReturn(expense);
+        when(participantStore.getParticipants(event.getId())).thenReturn(allUsers);
+        when(attendeeStore.getAttendees(expense.getId())).thenReturn(attendeeList);
+        when(payerAdapter.getSelectedUser()).thenReturn(payer);
+        when(payerAdapter.filterOutSelectedUser(any(List.class))).thenReturn(nonPayerList);
+        when(attendeeAdapter.getSelectedUsers()).thenReturn(attendeeSet);
+
+        Intent intent = new Intent();
+        intent.putExtra(ARGUMENT_EXPENSE_ID, expense.getId());
+        setActivityIntent(intent);
+
+        // When
+        getActivity();
+
+        // Then
+        verify(payerAdapter, times(1)).select(eq(payer));
+        for (User user : attendeeList) {
+            verify(attendeeAdapter, times(1)).select(eq(user));
+        }
+        onView(withId(R.id.expense_description)).check(matches(CustomViewMatchers.editTextWithText(expense.getDescription())));
+        onView(withId(R.id.expense_amount)).check(matches(CustomViewMatchers.editTextWithText(String.valueOf(expense.getAmount()))));
+
     }
 
     private static Matcher<Attendee> newAttendeeWithUserId(final String userId){
