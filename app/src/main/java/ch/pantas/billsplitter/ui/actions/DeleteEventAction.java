0000d0463c2bb7f2d6550456d6eb1c69babf9cd3
gigerdo <dominik.giger@gmail.com>
2014-10-22 17:25:02 +0200

    ADDED: Verify delete dialog box

--- app/src/main/java/ch/pantas/billsplitter/ui/actions/DeleteEventAction.java
+++ app/src/main/java/ch/pantas/billsplitter/ui/actions/DeleteEventAction.java
@@ -1,12 +1,15 @@
 package ch.pantas.billsplitter.ui.actions;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
 import com.google.inject.Inject;
 
 import ch.pantas.billsplitter.model.Event;
 import ch.pantas.billsplitter.services.ActivityStarter;
 import ch.pantas.billsplitter.services.EventService;
 import ch.pantas.billsplitter.ui.EventDetails;
-
+import ch.yvu.myapplication.R;
 
 public class DeleteEventAction implements EventDetailsAction {
 
@@ -17,7 +20,30 @@ public class DeleteEventAction implements EventDetailsAction {
     private ActivityStarter activityStarter;
 
     @Override
-    public boolean execute(EventDetails activity) {
+    public boolean execute(final EventDetails activity) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setMessage(activity.getResources().getString(R.string.delete_event_alert_message))
+                .setCancelable(false)
+                .setNegativeButton(activity.getResources().getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                })
+                .setPositiveButton(activity.getResources().getString(R.string.delete), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        deleteEvent(activity);
+                    }
+                })
+                .setTitle(activity.getResources().getString(R.string.delete_event_alert_title))
+                .setIcon(android.R.drawable.ic_dialog_alert);
+        AlertDialog alert = builder.create();
+        alert.show();
+
+        return true;
+    }
+
+    public void deleteEvent(EventDetails activity) {
         Event event = activity.getEvent();
         if(event != null){
             Event newActiveEvent = eventService.removeEventAndGetActiveEvent(event);
@@ -28,6 +54,5 @@ public class DeleteEventAction implements EventDetailsAction {
                 activityStarter.startEventDetails(activity, newActiveEvent, false);
             }
         }
-        return true;
     }
 }
