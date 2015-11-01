22fc429a78b3d136dce144d5fd187ed971c2d274
Yves Bonjour <yves.bonjour@gmail.com>
2015-11-01 11:54:30 +0100

    Remoes unused imports

--- app/src/main/java/ch/pantas/billsplitter/ui/NewGroupDialog.java
+++ app/src/main/java/ch/pantas/billsplitter/ui/NewGroupDialog.java
@@ -2,18 +2,11 @@ package ch.pantas.billsplitter.ui;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.view.View;
-import android.view.Window;
-import android.widget.Button;
 
 import com.google.inject.Inject;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import ch.pantas.billsplitter.services.ActivityStarter;
 import ch.pantas.splitty.R;
